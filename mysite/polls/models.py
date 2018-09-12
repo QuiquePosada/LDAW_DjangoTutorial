63cf3a1b2ba5a828fe12dc694d387b52dc773cbf
Quique Posada <quiqueposadalozano@Quique-Posadas-MacBook-Pro.local>
2018-09-12 12:45:17 -0500

    Tutorial_5_Completed

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,7 +12,9 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 	def was_published_recently(self):
-		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		# return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -20,3 +22,7 @@ class Choice(models.Model):
 	votes = models.IntegerField(default=0)
 	def __str__(self):
 		return self.choice_text
+
+# def was_published_recently(self):
+    # now = timezone.now()
+    # return now - datetime.timedelta(days=1) <= self.pub_date <= now
